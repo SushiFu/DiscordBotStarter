08ccfebacc44e34bd0b148f6610ae7c080f8ba69
SushiFu <SushiFu@gmx.fr>
2017-05-05 02:05:38 +1000

    :bento: Rename logger

--- src/server/logger.js
+++ src/server/logger.js
@@ -2,7 +2,7 @@ import bunyan from "bunyan";
 import config from "./config";
 
 const logger = new bunyan.createLogger({
-    name: "BaleineBot",
+    name: "DiscordBotStarter",
     stream: process.stdout,
     level: config.LOG_LEVEL
 });
